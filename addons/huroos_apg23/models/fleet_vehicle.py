5d03397403b9327475a5df7df683a09f8768e629
Vanni Matteo <vannicraft@gmail.com>
2026-03-13 14:47:15 +0100

    Update fleet_vehicle.py

--- addons/huroos_apg23/models/fleet_vehicle.py
+++ addons/huroos_apg23/models/fleet_vehicle.py
@@ -1,7 +1,7 @@
 import json
 from odoo import fields, models, api, Command
 import datetime
-# TEST DA MATTEO
+# TEST DA MATTEO 2
 LIST_FUEL_TYPE = [('MISCELA', 'Miscela'),
                   ('BENZINA VERDE', 'Benzina Verde'),
                   ('G.P.L.', 'G.P.L'),
